823c4212e001fd58dd7ef0d551e61adc9455ce3b
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 02:06:37 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "17453d95-3e7d-4835-8eb8-f64fac8fc4ed",
+    "home_feed": [
+        {
+            "id": "167ae97a-dafa-4118-9e2b-09c83ce479b0",
+            "artist_name": "series",
+            "home_carousel": [
+                {
+                    "id": "f218360d-b854-4785-a9a8-141822a4ac5a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/81.png"
+                },
+                {
+                    "id": "29f63eaa-377f-4bbc-8463-040f0ff530d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png"
+                },
+                {
+                    "id": "11728294-2bfb-4cfa-b0b5-c7edd38a285a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/148.png"
+                },
+                {
+                    "id": "d9ad246e-63cb-4035-a242-bc879a3dbb49",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "3ca8a0ea-4b57-48a7-a742-f9edc58f1ac3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/242.png"
+                }
+            ],
+            "art_caption": "four",
+            "art_description": "Walk speak hotel. Method impact south bad month. Poor staff person site defense month drop."
+        },
+        {
+            "id": "b2cfa4b5-9feb-435a-8b5c-48fb1057c6fa",
+            "artist_name": "almost",
+            "home_carousel": [
+                {
+                    "id": "03bb79a4-b654-42e8-8f2a-1bfe4e03a356",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/47.png"
+                },
+                {
+                    "id": "8a97603f-35f9-4b18-9dfb-0c4749ba7e04",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                },
+                {
+                    "id": "311df2df-7375-4571-a90a-f74af431ab98",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/325.png"
+                },
+                {
+                    "id": "b57dcd4d-cca0-4c1e-982c-6a1a255ec216",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/145.png"
+                },
+                {
+                    "id": "0dfb6e4c-f554-4709-b069-e847cd9b7804",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/130.png"
+                }
+            ],
+            "art_caption": "say",
+            "art_description": "Western something prepare music. Decide management simply new. Center education anyone power explain behind defense."
+        },
+        {
+            "id": "ed02998e-8ad8-4223-b630-32e78097cbdd",
+            "artist_name": "cold",
+            "home_carousel": [
+                {
+                    "id": "655bb941-43d2-4ede-a6d4-2b742b30fb95",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/341.png"
+                },
+                {
+                    "id": "3e72ec03-1bd7-4e01-ab33-0033ddae8b6c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/229.png"
+                },
+                {
+                    "id": "001ba6d5-59c0-4270-b982-50b6b20e42cf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/179.png"
+                },
+                {
+                    "id": "3c62213f-1a05-4bb0-820b-85ee8084ca69",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "16463f48-4a23-4202-aaae-58ae30f080e4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                }
+            ],
+            "art_caption": "life",
+            "art_description": "Treat because help state per. Leg commercial level. Never expect black special break under."
+        },
+        {
+            "id": "c47e8a13-7e8d-4389-939e-740652ee3869",
+            "artist_name": "herself",
+            "home_carousel": [
+                {
+                    "id": "2a74d228-3045-426e-8d21-2eeb23ca4694",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/133.png"
+                },
+                {
+                    "id": "c351458a-46f5-4709-bcd7-cb18f447ae06",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "532d5b28-2873-467a-8f44-7b9aa8b38d1a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "246c42cd-11bd-4cdd-bc08-a5656cf1b70c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "4fc83c27-8e11-4f16-af56-37681eb6e4b6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/350.png"
+                }
+            ],
+            "art_caption": "image",
+            "art_description": "Law represent power heart. Sell power attorney walk spend."
+        },
+        {
+            "id": "7bc2d103-d754-4fb2-9c9c-2107befee89b",
+            "artist_name": "high",
+            "home_carousel": [
+                {
+                    "id": "1ec2f46b-2175-4fbc-abf3-2809480ec783",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/8.png"
+                },
+                {
+                    "id": "32afb822-7c21-4d43-870b-6219c868c17a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "8ed8b215-fde6-4fd7-9885-5908c4fe697f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/173.png"
+                },
+                {
+                    "id": "728f13cf-1b90-4e35-acb8-50cab970227b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/332.png"
+                },
+                {
+                    "id": "c62cfc24-1ee0-47e6-84b8-bd2ef1e8ca31",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/243.png"
+                }
+            ],
+            "art_caption": "nation",
+            "art_description": "Position blue policy step on product himself. Face something produce cell five decide. Together so some edge yard respond."
+        }
+    ]
+}
